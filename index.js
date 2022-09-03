96a2234a64ca5dbc9b0bb8dfe3155e0f9b3e3ee9
Romina Polo <rominalpolo@gmail.com>
2022-09-03 09:42:53 -0400

    lab

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = []
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length = this.items.length
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if (pos > this.items.length - 1){
+      throw new Error('OutOfBounds');
+    } else {
+    return this.items[pos];
+  }
+}
 
-  min() {}
+  max() {
+    if (this.items.length > 0) {
+      return Math.max(...this.items)
+    } else {
+throw new Error('EmptySortedList');
 
-  sum() {}
+  }
+}
+
+  min() {
+    if (this.items.length > 0) {
+      return Math.min(...this.items)
+    } else {
+throw new Error('EmptySortedList');
+  }
+}
+  
+
+  sum() {
+    let sum = 0
+    for (let i = 0; i < 
+  this.items.length; i += 1) {
+    sum += this.items[i];
+  }
 
   avg() {}
 }
+}
+
 
 module.exports = SortedList;
